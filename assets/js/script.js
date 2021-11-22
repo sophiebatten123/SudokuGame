cbae9c14a520104a80ae28696acf61eebd469517
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2021-11-22 12:36:52 +0000

    Add initial elements to JavaScript to make targetting elements easier and clearer

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+//Wait for the DOM to finish loading before running the game
+//Get the button elements and add event listeners to them
+
+let easyGame = []
+let mediumGame = []
+let hardGame = []
+
+let easyGameSolution = []
+let mediumGameSolution = []
+let hardGameSolution = []
+
+document.addEventListener("DOMContentLoaded", function(){
+    let userLogin = document.getElementById("login");
+    let instructionsManual = document.getElementById("instructions");
+    let highScores = document.getElementById("personal-best");
+    let playButton = document.getElementById("play");
+    let resetButton = document.getElementById("reset");
+})
